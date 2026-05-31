854e3feb295a24802513e93da5084ed94528d1eb
karan <karensefaie@gmail.com>
2026-05-31 15:27:32 +0330

    Update database.php

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver'   => 'mysql',
+    'host'     => '127.0.0.1',
+    'port'     => 3306,
+    'database' => 'mydb',
+    'username' => 'root',
+    'password' => 'secret',
+    'charset'  => 'utf8mb4',
+    'options'  => [],
+];
